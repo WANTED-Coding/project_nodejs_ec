f7e6a586555f1799ae91b34ae689467278620dda
lambiengcode <lambiengcode@gmail.com>
2021-04-14 03:16:38 +0700

    [fix] fix bodyParser and fix ecosystem

--- app/index.ts
+++ app/index.ts
@@ -1,29 +1,30 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import morgan from 'morgan';
-import bodyParser from 'body-parser';
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import cors from "cors";
 // express
-import express, { Application, RequestHandler } from 'express';
+import express, { Application, RequestHandler } from "express";
 // important typings
-import Server from './common/Server';
+import Server from "./common/Server";
 
-import Controller from './Controllers/Controller';
-import AuthController from './Controllers/Auth.Controller';
+import Controller from "./Controllers/Controller";
+import AuthController from "./Controllers/Auth.Controller";
 
 const controllers: Array<Controller> = [new AuthController()];
 const globalMiddleware: Array<RequestHandler> = [
-	bodyParser.json(),
-	morgan('combined')
+  bodyParser.urlencoded({ extended: false }),
+  cors({ origin: true }),
+  morgan("combined"),
 ];
 
 const PORT: number = Number(process.env.PORT!);
 const server: Server = new Server(PORT, `${process.env.DB_URL}`);
 
 Promise.resolve()
-	.then(() => server.initDatabase())
-	.then(() => {
-		server.loadMiddleware(globalMiddleware);
-		server.loadControllers(controllers);
-		const httpServer = server.run();
-	});
-
+  .then(() => server.initDatabase())
+  .then(() => {
+    server.loadMiddleware(globalMiddleware);
+    server.loadControllers(controllers);
+    const httpServer = server.run();
+  });
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,19 @@
 module.exports = {
-    apps: [
-      {
-        name: 'EC-21',
-        script: 'dist/index.js',
-        args: 'index.js',
-        wait_ready: true,
-        error_file: './logs/err.log',
-        out_file: './logs/out.log',
-        log_file: './logs/combined.log',
-        log_date_format: 'YYYY-MM-DD HH:mm:ss:SSSS',
-        min_uptime: 10000,
-        max_restarts: 3,
+  apps: [
+    {
+      name: "EC-21",
+      script: "./app/index.ts",
+      node_args: ["ts-node-dev", "--poll"],
+      autorestart: true,
+      instances: 1,
+      exec_interpreter: "node",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "development",
       },
-    ],
-  };
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
