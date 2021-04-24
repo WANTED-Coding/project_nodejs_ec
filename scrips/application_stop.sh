84cffa77dd6899f0191ffa8d91f18bc9077fc920
hongvinhmobile <lambiengcode@gmail.com>
2021-04-25 03:43:07 +0700

    [test] test auto deploy ec2 run pm2

--- scrips/application_stop.sh
+++ scrips/application_stop.sh
@@ -1,5 +1,8 @@
 
 #!/bin/bash
 #Stopping existing node servers
+sudo service codedeploy-agent stop
+/opt/codedeploy-agent/deployment-root
+sudo service codedeploy-agent start
 echo "Service already for start on server..."
 pm2 stop 0
