4e42cdc6be08b4abcda486b686500cf4b162b64f
hongvinhmobile <lambiengcode@gmail.com>
2021-04-25 00:44:58 +0700

    [fix] fix and test auto deploy ec2

--- scrips/application_start.sh
+++ scrips/application_start.sh
@@ -6,11 +6,6 @@ sudo chmod -R 777 /home/ubuntu/file/ec-2021
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/file/ec-2021
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
 #install node modules
 yarn
 
--- scrips/before_install.sh
+++ scrips/before_install.sh
@@ -1,10 +1,3 @@
-#!/bin/bash
-
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
-
 #create our working directory if it doesnt exist
 DIR="/home/ubuntu/file/ec-2021"
 if [ -d "$DIR" ]; then
