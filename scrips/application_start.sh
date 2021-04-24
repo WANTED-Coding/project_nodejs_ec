34da39e623ee5cd473a9e18176311645166e1ed5
hongvinhmobile <lambiengcode@gmail.com>
2021-04-25 00:29:11 +0700

    [config] done auto deploy ec2

---
+++ scrips/application_start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ubuntu/file
+
+#navigate into our working directory where we have all our github files
+cd /home/ubuntu/file
+
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+#install node modules
+yarn
+
+#start our node app in the background
+yarn pm2 
---
+++ scrips/application_stop.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+#Stopping existing node servers
+echo "Stopping any existing node servers"
+pm2 delete 0
