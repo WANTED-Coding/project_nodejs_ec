b1706a3381e525984cd0fb1ecbb412398ba886df
hongvinhmobile <lambiengcode@gmail.com>
2021-04-28 03:50:46 +0700

    [config] application_stop shell

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 #Stopping existing node servers
-echo "stop docker service..."
+echo "delete and rebuild docker image..."
 sudo docker rmi -f lambiengcode/ec-2021:latest
 sudo docker build -t lambiengcode/ec-2021:latest .
