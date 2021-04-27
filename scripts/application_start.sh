c1afd276aeb92c4b3a63969cabdf941cbc8ea2a1
hongvinhmobile <lambiengcode@gmail.com>
2021-04-28 03:52:17 +0700

    [config] application_start shell

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,6 +7,9 @@ sudo chmod -R 777 /home/ubuntu/file/ec-2021/dist
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/file/ec-2021
 
+# Build docker image
+sudo docker build -t lambiengcode/ec-2021:latest .
+
 # Run docker container
 sudo docker rm ec-2021
 sudo docker run -d --name cnpm  -p 3000:3000 --env-file .env lambiengcode/ec-2021
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -3,4 +3,3 @@
 #Stopping existing node servers
 echo "delete and rebuild docker image..."
 sudo docker rmi -f lambiengcode/ec-2021:latest
-sudo docker build -t lambiengcode/ec-2021:latest .
