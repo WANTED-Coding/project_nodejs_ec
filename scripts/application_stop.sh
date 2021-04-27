1ba5492d036852c293c5cd43ce6de99fc0921def
hongvinhmobile <lambiengcode@gmail.com>
2021-04-28 03:40:44 +0700

    [config] auto deploy

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
 #Stopping existing node servers
-# echo "Service already for start on server"
-# sudo docker rmi -f lambiengcode/ec-2021:latest
-# sudo docker build -t lambiengcode/ec-2021:latest .
+echo "stop docker service..."
+sudo docker rmi -f lambiengcode/ec-2021:latest
+sudo docker build -t lambiengcode/ec-2021:latest .
